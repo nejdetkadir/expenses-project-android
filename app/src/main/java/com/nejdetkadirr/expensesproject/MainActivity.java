7a0c985b0dd412ae8619a34bc202daa2c8917c78
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-14 16:25:29 +0300

    Read data from API

--- app/src/main/java/com/nejdetkadirr/expensesproject/MainActivity.java
+++ app/src/main/java/com/nejdetkadirr/expensesproject/MainActivity.java
@@ -4,11 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.nejdetkadirr.expensesproject.service.ExpensesAPI;
+import com.nejdetkadirr.expensesproject.service.ExpensesModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    CompositeDisposable compositeDisposable;
+    ArrayList<ExpensesModel> expensesModelArrayList;
+    private String baseURL = "https://api.nejdetkadirbektas.com/";
+    Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new GsonBuilder().setLenient().create();
+        retrofit = new Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        loadDataFromAPI();
     }
+
+    private void loadDataFromAPI() {
+        ExpensesAPI expensesAPI = retrofit.create(ExpensesAPI.class);
+        compositeDisposable = new CompositeDisposable();
+        compositeDisposable.add(expensesAPI.getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse));
+    }
+
+    private void handleResponse(List<ExpensesModel> expensesModelList) {
+        expensesModelArrayList = new ArrayList<>(expensesModelList);
+        for (ExpensesModel e: expensesModelArrayList) {
+            System.out.println(e.category);
+        }
+    }
+
 }
